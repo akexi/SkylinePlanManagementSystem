9259e177aa8dd8e8deb8d86d55494fe1f9cc2c54
akexi <xsbnltx@gmail.com>
2026-04-10 01:25:07 +0800

    feat: 课程管理，新增课程的查看、编辑、删除功能

--- MockSchoolManagement/Controllers/CourseController.cs
+++ MockSchoolManagement/Controllers/CourseController.cs
@@ -15,16 +15,17 @@ namespace MockSchoolManagement.Controllers
     {
         private readonly IRepository<Course, int> _courseRepository;
         private readonly IRepository<Department, int> _departmentRepository;
+        private readonly IRepository<CourseAssignment, int> _courseAssignmentRepository;
         private readonly ICourseService _courseService;
 
-        public CourseController(IRepository<Course, int> courseRepository, IRepository<Department, int> departmentRepository, ICourseService courseService)
+        public CourseController(IRepository<Course, int> courseRepository, IRepository<Department, int> departmentRepository, IRepository<CourseAssignment, int> courseAssignmentRepository, ICourseService courseService)
         {
             _courseRepository = courseRepository;
             _departmentRepository = departmentRepository;
+            _courseAssignmentRepository = courseAssignmentRepository;
             _courseService = courseService;
         }
 
-        // 不写[HttpGet]，默认就是GET请求
         public async Task<ActionResult> Index(GetCourseInput input)
         {
             var models = await _courseService.GetPaginatedResult(input);
@@ -81,7 +82,102 @@ namespace MockSchoolManagement.Controllers
             return dtos;
         }
 
+        #region 编辑功能
 
+        [HttpGet]
+        public IActionResult Edit(int? courseId)
+        {
+            if (!courseId.HasValue)
+            {
+                ViewBag.ErrorMessage = $"课程编号为{courseId}的信息不存在，请重试";
+                return View("NotFound");
+            }
+
+            var course = _courseRepository.FirstOrDefault(a => a.CourseId == courseId);
+
+            if (courseId == null)
+            {
+                ViewBag.ErrorMessage = $"课程编号为{courseId}的信息不存在，请重试";
+                return View("NotFound");
+            }
+
+            // 将学院列表中选中的值修改为true
+            var dtos = DepartmentsDropDownList(course.DepartmentId);
+            CourseCreateViewModel courseCreateViewModel = new CourseCreateViewModel
+            {
+                DepartmentList = dtos,
+                CourseId = course.CourseId,
+                Credits = course.Credits,
+                Title = course.Title,
+                DepartmentId = course.DepartmentId
+            };
+
+            return View(courseCreateViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(CourseCreateViewModel input)
+        {
+            if (ModelState.IsValid)
+            {
+                var course = _courseRepository.FirstOrDefault(a => a.CourseId == input.CourseId);
+
+                if (course != null)
+                {
+                    course.CourseId = input.CourseId;
+                    course.Credits = input.Credits;
+                    course.DepartmentId = input.DepartmentId;
+                    course.Title = input.Title;
+                    _courseRepository.Update(course);
+                    return RedirectToAction(nameof(Index)); // 返回列表页
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = $"课程编号为{input.CourseId}的信息不存在，请重试";
+                    return View("NotFound");
+                }
+            }
+
+            return View(input);
+        }
+
+        #endregion 编辑功能
+
+
+        #region 删除功能
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var model = await _courseRepository.FirstOrDefaultAsync(a => a.CourseId == id);
+
+            if(model == null)
+            {
+                ViewBag.ErrorMessage = $"课程编号为{id}的信息不存在，请重试";
+                return View("NotFound");
+            }
+
+            await _courseAssignmentRepository.DeleteAsync(a => a.CourseId == id);   // 这里暂时有问题，如果课程没有教师分配记录，就会报错，后续再优化
+            await _courseRepository.DeleteAsync(a => a.CourseId == id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        #endregion 删除功能
+
+        [HttpGet]
+        public async Task<ViewResult> Details(int courseId)
+        {
+            var course = await _courseRepository.GetAll().Include(a => a.Department).FirstOrDefaultAsync(a => a.CourseId == courseId);
+
+            if(course == null)
+            {
+                ViewBag.ErrorMessage = $"课程编号为{courseId}的信息不存在，请重试";
+                return View("NotFound");
+            }
+
+            return View(course);
+        }
 
 
     }
