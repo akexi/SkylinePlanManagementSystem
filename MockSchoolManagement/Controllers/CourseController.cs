7252b767ef75d3701c054a68a2d173472ff36dab
OfficePC <1827961898@qq.com>
2026-04-09 17:31:40 +0800

    feat: 课程管理，新增添加课程功能

--- MockSchoolManagement/Controllers/CourseController.cs
+++ MockSchoolManagement/Controllers/CourseController.cs
@@ -1,17 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using MockSchoolManagement.Application.Courses;
 using MockSchoolManagement.Application.Courses.Dtos;
 using MockSchoolManagement.DataRepositories;
 using MockSchoolManagement.Infrastructure;
+using MockSchoolManagement.Infrastructure.Repositories;
+using MockSchoolManagement.Models;
+using MockSchoolManagement.ViewModels.Courses;
 
 namespace MockSchoolManagement.Controllers
 {
     public class CourseController : Controller
     {
+        private readonly IRepository<Course, int> _courseRepository;
+        private readonly IRepository<Department, int> _departmentRepository;
         private readonly ICourseService _courseService;
 
-        public CourseController(ICourseService courseService)
+        public CourseController(IRepository<Course, int> courseRepository, IRepository<Department, int> departmentRepository, ICourseService courseService)
         {
+            _courseRepository = courseRepository;
+            _departmentRepository = departmentRepository;
             _courseService = courseService;
         }
 
@@ -21,5 +30,59 @@ namespace MockSchoolManagement.Controllers
             var models = await _courseService.GetPaginatedResult(input);
             return View(models);
         }
+
+        #region 添加课程
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var dtos = DepartmentsDropDownList();
+            CourseCreateViewModel courseCreateViewModel = new CourseCreateViewModel
+            {
+                DepartmentList = dtos
+            };
+
+            // 将DepartmentsDropDownList方法的SelectList返回值添加到CourseCreateViewModel中，传递到视图中
+            return View(courseCreateViewModel);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CourseCreateViewModel input)
+        {
+            if (ModelState.IsValid)
+            {
+                Course course = new Course
+                {
+                    CourseId = input.CourseId,
+                    Title = input.Title,
+                    Credits = input.Credits,
+                    DepartmentId = input.DepartmentId
+                };
+
+                await _courseRepository.InsertAsync(course);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        #endregion 添加课程
+
+        /// <summary>
+        /// 学院的下拉列表
+        /// </summary>
+        /// <param name="selectedDepartmentId"></param>
+        private SelectList DepartmentsDropDownList(object selectedDepartment = null)
+        {
+            var models = _departmentRepository.GetAll().OrderBy(a => a.Name).AsNoTracking().ToList();
+            var dtos = new SelectList(models, "DepartmentId", "Name", selectedDepartment);
+
+            return dtos;
+        }
+
+
+
+
     }
 }
---
+++ MockSchoolManagement/ViewModels/Courses/CourseCreateViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace MockSchoolManagement.ViewModels.Courses
+{
+    public class CourseCreateViewModel
+    {
+        [Display(Name = "课程编号")]
+        public int CourseId { get; set; }
+
+        [Display(Name = "课程名称")]
+        public string Title { get; set; }
+
+        [Display(Name = "学分")]
+        public int Credits { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        [Display(Name = "学院")]
+        public SelectList? DepartmentList { get; set; }
+    }
+}
