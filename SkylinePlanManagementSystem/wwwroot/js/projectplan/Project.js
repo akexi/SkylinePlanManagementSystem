eb2a0feef1908790687ff3e004d2095c00432348
OfficePC <1827961898@qq.com>
2026-04-30 10:54:35 +0800

    feat: 1、修改节点视图小改

---
+++ SkylinePlanManagementSystem/wwwroot/js/projectplan/Project.js
@@ -0,0 +1,25 @@
+﻿document.querySelectorAll(".btn-edit").forEach(btn => {
+    btn.addEventListener("click", function () {
+        let tr = this.closest("tr");
+
+        tr.querySelectorAll(".view-mode").forEach(x => x.classList.add("d-none"));
+        tr.querySelectorAll(".edit-mode").forEach(x => x.classList.remove("d-none"));
+
+        tr.querySelector(".btn-edit").classList.add("d-none");
+        tr.querySelector(".btn-save").classList.remove("d-none");
+        tr.querySelector(".btn-cancel").classList.remove("d-none");
+    });
+});
+
+document.querySelectorAll(".btn-cancel").forEach(btn => {
+    btn.addEventListener("click", function () {
+        let tr = this.closest("tr");
+
+        tr.querySelectorAll(".view-mode").forEach(x => x.classList.remove("d-none"));
+        tr.querySelectorAll(".edit-mode").forEach(x => x.classList.add("d-none"));
+
+        tr.querySelector(".btn-edit").classList.remove("d-none");
+        tr.querySelector(".btn-save").classList.add("d-none");
+        tr.querySelector(".btn-cancel").classList.add("d-none");
+    });
+});
